296be751cbbdb1565b9c5520cb513b35d52be677
pratik-sedulous <pratik.sedulous@gmail.com>
2019-09-03 13:57:08 +0530

    Extend the form functionality to be able to update question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,20 +37,22 @@
                                     <h3 class="mt-0">
                                         <a href="{{ $question->url }}">{{ $question->title }}</a>
                                     </h3>
-                                    {{--
+
                                     <div class="ml-auto">
-                                        @can ('update', $question)
+                                        {{-- @can ('update', $question) --}}
                                             <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can ('delete', $question)
+                                        {{-- @endcan --}}
+                                        {{-- @can ('delete', $question) --}}
+                                        {{--
                                             <form action="{{ route('questions.destroy', $question->id) }}" method="post" class="form-delete">
                                                 @method('DELETE')
                                                 @csrf
                                                 <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                             </form>
-                                        @endcan
+                                        --}}
+                                        {{-- @endcan --}}
                                     </div>
-                                    --}}
+
                                 </div>
                                 
                                 <p class="lead">
